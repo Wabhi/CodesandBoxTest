c49864e4b6d6d85a6e5e58db239f3725b4c58a02
ABHISHEK TIWARI <tiwariabhishek27611@gmail.com>
2021-05-21 17:20:46 +0530

    counter changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+  const handleDecrement = () => {
+    setCounter((newCount) => {
+      let newCounter = newCount - 1;
+      if (newCount === 0) {
+        newCounter = 0;
+      }
+      return newCounter;
+    });
+  };
+  const handleReset = () => {
+    setCounter(0);
+  };
   return (
     <div className="App">
       <h1>Hello Abhishek Tiwari</h1>
-      <h2>I am ready to work with codesandbox.</h2>
+      <h4>{counter}</h4>
+      <button onClick={handleIncrement}>INCREMENT</button>
+      <button onClick={handleDecrement}>DECREMENT</button>
+      <button onClick={handleReset}>RESET</button>
     </div>
   );
 }
