975c1bdf7fff826d1d163b6948cbd7e328ceb922
ABHISHEK TIWARI <tiwariabhishek27611@gmail.com>
2021-05-21 17:06:39 +0530

    changing the title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello Abhishek Tiwari</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>I am ready to work with codesandbox.</h2>
     </div>
   );
 }
